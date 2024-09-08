d6ef8fed0a39ea15628520aa921f36465a7bff0e
Ben Crapo <bencrapo@icloud.com>
2024-09-07 18:03:51 -0600

    Modified Hello World class to reflect new things I learned about Java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
+
+import java.util.Random; //how we get classes from specific packages. (like libraries, but with hierarchy)
+
 public class HelloWorld {
     public static void main(String[] args) {
+        Random generate = new Random(); // how we instance classes
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!".length()); //even constants are classes with methods inside.
+        System.out.println(generate.nextInt());
+        System.out.println(generate.nextInt());
     }
 }
